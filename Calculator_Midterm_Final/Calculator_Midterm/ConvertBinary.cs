d89d89efe1fdd94acd279a668e48b7242ccda6c8
Raviporn Sae-tiew <raviporn.sae-tiew@students.stamford.edu>
2017-09-04 22:14:59 +0700

    Convert_Binary

---
+++ Calculator_Midterm_Final/Calculator_Midterm/ConvertBinary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Midterm
+{
+	public partial class ConvertBinary : Form
+	{
+		public ConvertBinary()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int num = Convert.ToInt32(textBox1.Text);
+			int decimal_val = 0, base_val = 1, rem;
+
+			while (num > 0)
+			{
+				rem = num % 10;
+				decimal_val = decimal_val + rem * base_val;
+				num = num / 10;
+				base_val = base_val * 2;
+			}
+
+			textBox2.Text = Convert.ToString(decimal_val);
+		}
+	}
+}
