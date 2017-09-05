ccd14cecdc38d9b0e97a9625f4a95c3f1ebb3b76
Raviporn Sae-tiew <raviporn.sae-tiew@students.stamford.edu>
2017-09-05 09:17:35 +0700

    Cal_info

---
+++ Calculator_Midterm_Final/Calculator_Midterm/CalculatorInfo.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Calculator_Midterm
+{
+	partial class CalculatorInfo
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+			this.label3.Location = new System.Drawing.Point(136, 81);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(152, 26);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Calculator Info";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+			this.label2.Location = new System.Drawing.Point(23, 129);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(390, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "This calculator has the basic function of the culculator.";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 66);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 13);
+			this.label1.TabIndex = 3;
+			// 
+			// CalculatorInfo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(426, 263);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "CalculatorInfo";
+			this.Text = "CalculatorInfo";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
